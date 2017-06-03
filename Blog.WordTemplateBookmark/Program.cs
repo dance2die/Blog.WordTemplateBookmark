80f2033abddb8864576ef8f28f3a503f704e9325
dance2die <dontbothermewithspam@gmail.com>
2017-06-03 13:55:04 -0400

    Correctly populates bookmarks.

--- Blog.WordTemplateBookmark/Program.cs
+++ Blog.WordTemplateBookmark/Program.cs
@@ -1,15 +1,54 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Office.Interop.Word;
 
 namespace Blog.WordTemplateBookmark
 {
-	class Program
+	public class Program
 	{
-		static void Main(string[] args)
+		public static void Main(string[] args)
 		{
+			Application word = null;
+			Document document = null;
+
+			try
+			{
+				// Open Document
+				word = new Application();
+
+				var templatePath = Path.Combine(Directory.GetCurrentDirectory(), "BookmarkTest.docx");
+				document = word.Documents.Open(templatePath);
+
+				// Populate bookmarks.
+				const string signer = "Anna Bertha Cecilia Diana Emily Fanny Gertrude Hypatia Inez Jane ";
+				SetSignerBookmarks(word, document.Bookmarks, signer);
+
+				// Find table.
+				// calculate text length
+				// increase the table size
+			}
+			catch (Exception ex)
+			{
+				document?.Close();
+			}
+			finally
+			{
+				word?.Quit(WdSaveOptions.wdSaveChanges, WdOriginalFormat.wdOriginalDocumentFormat);
+			}
+		}
+
+		private static void SetSignerBookmarks(Application word, Bookmarks bookmarks, string signer)
+		{
+			// https://social.msdn.microsoft.com/Forums/vstudio/en-US/32b25cfd-cc5b-4e9f-bcbf-0dbbd49bca02/how-to-replace-text-or-insert-text-into-bookmarks-in-a-word-template?forum=csharpgeneral
+			bookmarks["signer1"].Select();
+			word.Selection.TypeText(signer);
+
+			bookmarks["signer2"].Select();
+			word.Selection.TypeText(signer);
 		}
 	}
 }
